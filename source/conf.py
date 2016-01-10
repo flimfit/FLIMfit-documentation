e1e689a0aa0cf388e88bd94d3f7312417fe5e8aa
Sean Warren <sean.warren09@imperial.ac.uk>
2016-01-10 21:20:30 +1100

    Abandon custom theme for the moment

--- source/conf.py
+++ source/conf.py
@@ -105,13 +105,11 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-#if not on_rtd:  # only import and set the theme if we're building docs locally
+if not on_rtd:  # only import and set the theme if we're building docs locally
     #import sphinx_rtd_theme
-html_theme = "custom_sphinx_rtd_theme"
-html_theme_path = ["_themes", ]
-    #html_theme_path = ["../.."]
-    #html_theme_path = ["_themes", ]
-
+    html_theme = "custom_sphinx_rtd_theme"
+    html_theme_path = ["_themes", ]
+    
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
